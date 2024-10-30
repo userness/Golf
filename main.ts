0fbac3927baf9303383090f5de88c0a0b01b7b77
userness <auth.microsoft.no.reply@gmail.com>
2024-10-30 10:41:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    if (stop == 0) {
+        stop = 1
+    } else {
+        stop = 0
+    }
+})
 let stop = 0
 hummingbird.startHummingbird()
 music.play(music.tonePlayable(880, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+stop = 0
+basic.showString("Grinch")
 basic.forever(function () {
     if (stop != 1) {
         hummingbird.setPositionServo(FourPort.One, 90)
@@ -12,14 +21,5 @@ basic.forever(function () {
 basic.forever(function () {
     if (stop != 1) {
         hummingbird.setRotationServo(FourPort.Two, 100)
-    }
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        if (stop == 0) {
-            stop = 1
-        } else {
-            stop = 0
-        }
     }
 })
